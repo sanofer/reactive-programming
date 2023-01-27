4b2b80633c2f833b80afe695f2631c1a182f4e84
Halima Sanofer <halimasanofer@gmail.com>
2023-01-27 10:27:01 -0500

    Code change for userFlux

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -8,11 +8,11 @@ public class Exercise2 {
 
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
         ReactiveSources.intNumbersFlux().subscribe(e -> System.out.println(e));
-        // ReactiveSources.userFlux().subscribe(e -> System.out.println(e));
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
         // TODO: Write code here
 
         // Print all users in the ReactiveSources.userFlux stream
+        ReactiveSources.userFlux().subscribe(user -> System.out.println(user));
         // TODO: Write code here
 
         System.out.println("Press a key to end");
